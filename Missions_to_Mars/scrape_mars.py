9ba15c9f15720ec31b49810d3b4355fbeaf7cc4e
Adela Garza <a.garza045@gmail.com>
2020-10-02 21:30:11 -0500

    updated vscode

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,79 +1,157 @@
-# Import Splinter, BeautifulSoup, and Pandas
-from splinter import Browser
-from bs4 import BeautifulSoup as soup
 import pandas as pd
-import datetime as dt
-def scrape_all():
-     
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    browser = Browser('chrome', **executable_path, headless=False)
-    news_title, news_paragraph = mars_news(browser)
-     
-    data = {
-        "news_title": news_title,
-        "news_paragraph": news_paragraph,
-        "featured_image": featured_image(browser),
-        "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
-    }
-    # Stop webdriver and return data
-    browser.quit()
-    return data
-def mars_news(browser):
-    # Scrape Mars News
-    
-    url = 'https://mars.nasa.gov/news/'
-    browser.visit(url)
-    # Delay for loading the page
-    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
-    
-    html = browser.html
-    news_soup = soup(html, 'html.parser')
-    # Add try/except for error handling
-    try:
-        slide_elem = news_soup.select_one("ul.item_list li.slide")
-        # Use the parent element to find the first 'a' tag and save it as 'news_title'
-        news_title = slide_elem.find("div", class_="content_title").get_text()
-        # Use the parent element to find the paragraph text
-        news_p = slide_elem.find("div", class_="article_teaser_body").get_text()
-    except AttributeError:
-        return None, None
-    return news_title, news_p
-def featured_image(browser):
-    # Visit URL
-    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-    browser.visit(url)
-    # Find and click the full image button
-    full_image_elem = browser.find_by_id('full_image')[0]
-    full_image_elem.click()
-    # Find the more info button and click that
-    browser.is_element_present_by_text('more info', wait_time=1)
-    more_info_elem = browser.find_link_by_partial_text('more info')
-    more_info_elem.click()
-    # Parse the resulting html with soup
-    html = browser.html
-    img_soup = soup(html, 'html.parser')
-    # Add try/except for error handling
-    try:
-        # Find the relative image url
-        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-    except AttributeError:
-        return None
-    # Use the base url to create an absolute url
-    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-    return img_url
-def mars_facts():
-    # Add try/except for error handling
-    try:
-        # Use 'read_html' to scrape the facts table into a dataframe
-        df = pd.read_html('http://space-facts.com/mars/')[0]
-    except BaseException:
-        return None
-    # Assign columns and set index of dataframe
-    df.columns=['Description', 'Mars']
-    df.set_index('Description', inplace=True)
-     
-    return df.to_html(classes="table table-striped")
-if __name__ == "__main__":
-     
-    print(scrape_all())
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+
+def scrap_all():
+
+executable_path = {'executable_path': 'chromedriver.exe'}
+browser = Browser('chrome', **executable_path, headless=False)
+news_title, news_paragraph = mars_news(browser)
+
+
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+
+#news_title = "NASA's Next Mars Mission to Investigate Interior of Red Planet"
+
+#news_p = "Preparation of NASA's next spacecraft to Mars, InSight, has ramped up this summer, on course for launch next May from Vandenberg Air Force Base in central California -- the first interplanetary launch in history from America's West Coast."
+
+html = browser.html
+soup = bs(html, 'html.parser')
+
+news_title_div = soup.find('div', class_='list_text').find('div', class_='content_title')
+
+news_title = news_title_div.get_text()
+
+news_title
+
+
+news_p_div = soup.find('div', class_='article_teaser_body')
+
+news_p_div
+
+news_p = news_p_div.get_text()
+
+news_p
+
+#JPL Mars Space Images - Featured Image
+#visit url
+url_jpl = 'https://www.jpl.nasa.gov/spaceimages//?search=&category=Mars'
+browser.visit(url_jpl)
+
+#find image button
+image = browser.find_by_id('full_image')
+image.click()
+
+#find more info button
+browser.is_element_present_by_text('more info', wait_time=1)
+
+find_button = browser.links.find_by_partial_text('more info')
+find_button.click()
+
+html = browser.html
+soup = bs(html, 'html.parser')
+
+image_url_a = soup.find('figure', class_='lede').find('img', class_='main_image').get('src')
+
+image_url_a
+
+base = 'https://www.jpl.nasa.gov'
+
+f_link_url = base + image_url_a
+browser.visit(f_link_url)
+
+f_link_url
+
+#Mars Facts
+table_url = 'https://space-facts.com/mars/'
+browser.visit(table_url)
+
+tables = pd.read_html(table_url)
+
+type(tables)
+
+list
+
+tables[0]
+
+df=tables[0]
+
+df.columns = ['Description', 'Mars']
+
+df.head()
+
+df.set_index('Description', inplace=True)
+df.head()
+
+df.to_html('table.html')
+
+#Mars Hemispheres
+hem_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+browser.visit(hem_url)
+
+base = 'https://astrogeology.usgs.gov/'
+
+#Cerberus Hemisphere Enhanced
+cer_hem = browser.links.find_by_partial_text('Cerberus Hemisphere Enhanced')
+cer_hem.click()
+
+html = browser.html
+soup = bs(html, 'html.parser')
+
+cer_hem_url = soup.find('img', class_='wide-image').get('src')
+
+mars_cer = base + cer_hem_url
+
+mars_cer
+
+#Schiaparelli Hemisphere Enhanced
+sch_hem = browser.links.find_by_partial_text('Schiaparelli Hemisphere Enhanced')
+sch_hem.click()
+
+html = browser.html
+soup = bs(html, 'html.parser')
+
+sch_hem_url = soup.find('img', class_='wide-image').get('src')
+
+mars_sch = base + cer_hem_url
+
+mars_sch
+
+#Syrtis Major Hemisphere Enhanced
+syr_hem = browser.links.find_by_partial_text('Syrtis Major Hemisphere Enhanced')
+syr_hem.click()
+
+html = browser.html
+soup = bs(html, 'html.parser')
+
+syr_hem_url = soup.find('img', class_='wide-image').get('src')
+
+mars_syr = base + cer_hem_url
+
+mars_syr
+
+#Valles Marineris Hemisphere Enhanced
+val_hem = browser.links.find_by_partial_text('Valles Marineris Hemisphere Enhanced')
+val_hem.click()
+
+html = browser.html
+soup = bs(html, 'html.parser')
+
+val_hem_url = soup.find('img', class_='wide-image').get('src')
+
+mars_val = base + val_hem_url
+
+mars_val
+
+hemisphere_image_urls = [
+    {"title": "Cerberus Hemisphere Enhanced", "img_url": mars_cer},
+    {"title": "Schiaparelli Hemisphere Enhanced", "img_url": mars_sch},
+    {"title": "Syrtis Major Hemisphere Enhanced", "img_url": mars_syr},
+    {"title": "Valles Marineris Hemisphere Enhanced", "img_url": mars_val},
+]
+
+hemisphere_image_urls
+
+
+# %%
